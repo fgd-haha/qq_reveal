782ea10884df0be68d37e33043bc700fd1cd0576
fgd <fgd.freedom@gmail.com>
2020-03-01 21:30:18 +0800

    脱敏

--- test/test_qq_reveal.py
+++ test/test_qq_reveal.py
@@ -13,9 +13,9 @@ def test_is_revealed():
     driver = webdriver.Chrome(r"C:\Users\fgdfr\Desktop\code\qq_reveal\config\chromedriver.exe")
     driver.maximize_window()
     driver.get('https://haveibeenpwned.com/')
-    assert True == qq_reveal.is_revealed(driver, '993264844@qq.com'), '993264844@qq.com 应该为已泄露账号'
+    assert True == qq_reveal.is_revealed(driver, '*******@qq.com'), '********@qq.com 应该为已泄露账号'
 
 
 def test_reveal_check():
-    qq_list = {'839684679@qq.com': '前尘忆梦', 'kuaile_tiantian@qq.com': 'kuaile_tiantian'}
+    qq_list = {'*********@qq.com': '前尘忆梦', '*****@qq.com': 'kuaile_tiantian'}
     qq_reveal.reveal_check(qq_list)
